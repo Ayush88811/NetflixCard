948295ef0b672d66768294ebb64a60276c7ab8be
Ayush88811 <ayushverma34104@gmail.com>
2025-09-23 22:14:59 +0530

    Registration form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import { Counter } from "./components/hooks/useState";
 import CounterChallenge from "./components/hooks/useState/Challenge";
+import { RegistrationForm } from "./components/hooks/useState/Registration";
+import { RegistrationFormReact } from "./components/hooks/useState/RegistrationReact";
 
 
 
 
  export const App=()=> {
-  return<CounterChallenge/>;
+  return<RegistrationFormReact/>;
  };
  
  export default App;
