e904c22d529cb13d8ef99f113902bb173b9c4e6e
Ayush88811 <ayushverma34104@gmail.com>
2025-10-04 22:45:36 +0530

    4 oct

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 
+import { ReactUseEffect } from "./components/hooks/UseEffect";
+import { CleanUp } from "./components/hooks/UseEffect/CleanUp";
+import { Challenge } from "./components/hooks/UseEffect/EffectChallenge";
+import { HowNotToFetchApi } from "./components/hooks/UseEffect/HowNotToFetchApi";
+import { UseId } from "./components/hooks/UseID";
+import { UseRef } from "./components/hooks/useRefs";
+import { ForwardRefs } from "./components/hooks/useRefs/ForwardRef";
 import { Counter } from "./components/hooks/useState";
 import CounterChallenge from "./components/hooks/useState/Challenge";
+import { ContactForm } from "./components/hooks/useState/ContactForm";
+import { LoginForm } from "./components/hooks/useState/LoginForm";
 import { RegistrationForm } from "./components/hooks/useState/Registration";
 import { RegistrationFormReact } from "./components/hooks/useState/RegistrationReact";
+import { ParentComponent } from "./components/PropDrilling";
 
 
 
 
  export const App=()=> {
-  return<RegistrationFormReact/>;
+  return <ParentComponent/>;
  };
  
  export default App;
---
+++ src/components/hooks/UseEffect/EffectChallenge.jsx
@@ -0,0 +1,29 @@
+import "./index.css";
+import { useState,useEffect } from "react";
+export const Challenge = ()=> {
+    const [count, setCount]=useState(0);
+    const [name,setName] = useState("ayush");
+
+    //order matters! keep Hooks in the same order across renders
+    useEffect(() => {
+        document.title = `count: ${count}`;
+    },[count]);
+
+    useEffect(() => {
+        console.log(name);
+    },[name]);
+    return(
+        <div className="container effect-container">
+            <h1>useEffect Challenge</h1>
+
+            <p>
+                Count: <span> {count} </span>
+            </p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <p>
+                Name: <span> {name} </span>
+            </p>
+            <input type="text" value = {name} onChange={(e) => setName(e.target.value)}/>
+        </div>
+    );
+};
